e5f3e51178a6ef386d5b022fbc0b26070b0a7f03
tao <13522338102@163.com>
2021-06-02 21:26:58 +0800

    第一次提交

---
+++ taobao/js/out.js
@@ -0,0 +1,26 @@
+alert("张海涛");
+
+
+console.log("张海涛");
+
+document.write("hello" + "<br>");
+
+var a = "李晓燕"
+
+console.log(a)
+
+
+var stuName = "hello" ,age ="18"
+
+
+document.write("我的名字叫" + stuName + "<br>" + "我的年龄" + age + "<br>");
+
+var i = 10,j;
+j = ++i + i++ + i
+alert(j)
+    
+var P = prompt("请输入一个数")
+
+alert (typeof P)
+
+document.write(100 + '100' - 10 + '20')
